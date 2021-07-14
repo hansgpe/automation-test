920bd97da58fbb7796f1a863b7a91d4b57129937
Gabriel Hans González Peña <hansgpe2@gmail.com>
2021-07-14 12:50:47 -0500

    Prueba de cypress

--- cypress/integration/search-test.js
+++ cypress/integration/search-test.js
@@ -2,7 +2,8 @@ describe('search in catalogo',()=>{
 it('search in catalog',()=>{
 cy.visit('localhost:3000/shows')
 cy.get('input').type('Batman')
-cy.get('.btn').click()
+cy.get('.btn').click();
+cy.get('.card').drag('.card[2]');
 cy.get('.card').contains('Batman Unlimited')
 })
 })
